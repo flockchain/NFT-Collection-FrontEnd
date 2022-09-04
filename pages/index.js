1ad48ce9dac1e1dcbeb7c44186f447b4275e49d0
felix_blockchainer <97702158+flockchain@users.noreply.github.com>
2022-09-04 16:32:52 +0200

    fixing public mint bug

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,36 @@ export default function Home()
     }
   };
 
+    /**
+   * publicMint: Mint an NFT after the presale
+   */
+     const publicMint = async () => {
+      try {
+        // We need a Signer here since this is a 'write' transaction.
+        const signer = await getProviderOrSigner(true);
+        // Create a new instance of the Contract with a Signer, which allows
+        // update methods
+        const whitelistContract = new Contract(
+          NFT_CONTRACT_ADDRESS,
+          abi,
+          signer
+        );
+        // call the mint from the contract to mint the Crypto Dev
+        const tx = await whitelistContract.mint({
+          // value signifies the cost of one crypto dev which is "0.01" eth.
+          // We are parsing `0.01` string to ether using the utils library from ethers.js
+          value: utils.parseEther("0.01"),
+        });
+        setLoading(true);
+        // wait for the transaction to get mined
+        await tx.wait();
+        setLoading(false);
+        window.alert("You successfully minted a Crypto Dev!");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
   //connectWallet: Connects the MetaMask wallet
   const connectWallet = async () => 
   {
